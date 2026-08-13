ab8b5595c7a56de68264fe116cf1536aee8431b9
sricharan-527 <charan.57k@gmail.com>
2026-08-13 17:03:46 +0530

    completed

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,27 @@
-alert("hello from js")
+//alert("hello from js")
+
+const users = [
+    {
+        "name" : "John Doe",
+        "gender" : "male",
+        "img" : "john.png"
+    }, 
+    {
+        "name" : "Jane Doe",
+        "gender" : "female",
+        "img" : "jane.png"
+    }
+]
+
+var curIndex = 0;
+
+function Toggle() {
+    if(curIndex == 0)
+        curIndex = 1;
+    else
+        curIndex = 0;
+
+    document.getElementById("card-name").innerText = users[curIndex].name;
+    document.getElementById("card-gender").innerText = users[curIndex].gender;
+    document.getElementById("card-image").src = users[curIndex].img;
+}

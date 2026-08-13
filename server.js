49aee28e9aa6de66a2079da9c32e2f6f7696fafa
sricharan-527 <charan.57k@gmail.com>
2026-08-13 15:33:54 +0530

    adding server

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express')
 
 const app = express()
 
-const port =8080
+const port = process.env.PORT || 8080
+
+app.use(express.static('frontend'))
 
 app.listen(port, function() {
     console.log("my app is running at http://localhost:" + port)

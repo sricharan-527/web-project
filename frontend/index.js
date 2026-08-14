c63f3b7b0f67ce593112b5afd33c9bf2630ce622
sricharan-527 <charan.57k@gmail.com>
2026-08-14 10:53:14 +0530

    adding random user

--- frontend/index.js
+++ frontend/index.js
@@ -25,3 +25,17 @@ function Toggle() {
     document.getElementById("card-gender").innerText = users[curIndex].gender;
     document.getElementById("card-image").src = users[curIndex].img;
 }
+
+function random() {
+    fetch("https://randomuser.me/api")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        var detail = data.results[0];
+        document.getElementById("card-image").src = detail.picture.large;
+        document.getElementById("card-gender").innerText = detail.gender;
+        var fullname = detail.name.title + " " + detail.name.first + " " + detail.name.last;
+        document.getElementById("card-name").innerText = fullname;
+    });
+}
